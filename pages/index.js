358a32e2a775ed39a4f09b7a5049249b610696dc
RayadoxAV <49849531+RayadoxAV@users.noreply.github.com>
2020-11-24 18:40:41 -0700

    Script para la mudanza de la base de datos

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,38 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import firebase from '../components/firebase';
 
+
+
 export default function Home() {
   const database = firebase.database();
+  // const database2 = firebase.firestore().collection('negocios');
 
-  const casa = database.ref('/negocios/0').once('value').then((snapshot) => {
-    console.log(snapshot.val());
-  })
+  /* function randomString(length, chars) {
+    var mask = '';
+    if (chars.indexOf('a') > -1) mask += 'abcdefghijklmnopqrstuvwxyz';
+    if (chars.indexOf('A') > -1) mask += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    if (chars.indexOf('#') > -1) mask += '0123456789';
+    if (chars.indexOf('!') > -1) mask += '~`!@#$%^&*()_+-={}[]:";\'<>?,./|\\';
+    var result = '';
+    for (var i = length; i > 0; --i) result += mask[Math.floor(Math.random() * mask.length)];
+    return result;
+  }
+
+  const cosa = async () => {
+    const casa = database.ref('/negocios').once('value').then((snapshot) => {
+      snapshot.val().forEach( async (negocio) => {
+          console.log(negocio);
+          const id = randomString(32, '#aA');
+          const data = {
+            ...negocio
+          }
+          const res = await database2.doc(id).set(data);
+      });
+    });
+    
+  }
+
+  cosa(); */
   return (
     <div className={styles.container}>
       <Head>
